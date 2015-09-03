24da028438e3700b05242351c691fb2cb0142088
thmplt <thmplt@idcmweb.com>
2015-09-03 15:41:51 -0400

    Conditional changes

--- php/wp_conditional.php
+++ php/wp_conditional.php
@@ -17,14 +17,18 @@ function thmplt_is_page($atts, $content = NULL ){
 	global $post;
 	
     $a = shortcode_atts( array(
+		'is' => '',
         'pids' => '',
 		'not' => '' 
     ), $atts );	
 	
 	$pids = !empty($a['pids']) ? explode(",", $a['pids']): NULL ;
+	$is = !empty($a['is']) ? explode(",", $a['is']): NULL ;	
 	$not = !empty($a['not']) ? explode(",", $a['not']): NULL ;
 	
-	$is_pid = is_array($pids) ? is_page($pids) : is_page();
+	$pids = is_array($is) ? $is : $pids; // $is takes precedence 
+	
+	$is_pid = is_array($pis) ? is_page($pids) : is_page();
 	$is_not_pid	= is_array($not) ? !is_page($not) : true;
 	
 	// if both it is on a page is true and it's not on a certain page is true
