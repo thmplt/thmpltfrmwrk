6e223b1d48694b87aa5830f84dfd16923adc58c4
thmplt <contact@idcmweb.com>
2017-08-07 08:26:47 -0400

    Recommit

--- thmplt/captions.php
+++ thmplt/captions.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Captions with extra functionality BOOM!
- * Since Version 
+ * Since Version 1.6
  */
  
  
  
  /**
- * Grab the lastet post types and build HTML around it 
+ * Shortcode that crates responsive captions
  */
 function thmplt_caption($atts, $content){
 
@@ -38,3 +38,50 @@ function thmplt_caption($atts, $content){
 	
 } #thmplt_caption
 add_shortcode('thmplt_caption', 'thmplt_caption');
+
+
+
+
+ /**
+ * Shortcode that crates responsive captions using "figure" semantics
+ */
+function thmplt_fig_caption($atts, $content){
+
+	extract( shortcode_atts( array(
+		'id' => '', // 
+		'class' => '',
+		'imgclass' => '',
+		'imgsrc' => '',
+		'hvr' => '',
+		'href' => '',
+		'title' => '',
+		'atagpos' => 'outside',
+	), $atts ) );
+	
+	$html = "<figure ";
+	$html .= (!empty($id)) ? "id='".$id."' ": "";
+	$html .= " class='tpf-fig-caption ".$class."' >";
+	//$html .= "<figure class='".$figclass."' >";
+	
+	$html .= (!empty($imgsrc)) ? "<img src='".$imgsrc."' alt='".$title."' class=' tpf-fig-caption-img ".$imgclass."' ".$datahvr." />": "";
+	$html .= "<figcaption class='tpf-fig-caption-text'>";
+	$html .= "<strong class='tpf-fig-caption-title'>".$title."</strong>";
+	$html .= (!empty($content)) ?  $content:"";
+	
+	if ($atagpos='inside') { 
+			$html .= (!empty($href)) ? "<a href='".$href."' ></a>": ""; 
+	}  
+	
+	$thml .= "</figcaption>";
+
+	if ($atagpos='outside') { 
+			$html .= (!empty($href)) ? "<a href='".$href."' ></a>": ""; 
+	}  
+	
+	$html .= "</figure>";
+	
+	return $html;
+	
+} #thmplt_caption
+add_shortcode('thmplt_fig_caption', 'thmplt_fig_caption');
+
